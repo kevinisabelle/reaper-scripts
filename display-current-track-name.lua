07db87e6493ef791a54994dc080a0fbce3514b7b
Kevin Isabelle <kev.isabelle@gmail.com>
2021-11-19 13:03:46 -0500

    updaets

--- display-current-track-name.lua
+++ display-current-track-name.lua
@@ -1,7 +1,12 @@
 local name = "Displays the tracks on the console"
 
 track = reaper.GetSelectedTrack(0, 0)
+if track == nil then 
+    reaper.ShowConsoleMsg("No tracks are selected")
+    return 
+end
 -- visibleChain = reaper.TrackFX_GetChainVisible(track)
 ret, trackname = reaper.GetTrackName(track)
-
-reaper.ShowConsoleMsg("Track: "..trackname)
+buf = ""
+ret, fxname = reaper.TrackFX_GetFXName(track, 0, buf)
+reaper.ShowConsoleMsg("Track: "..trackname..", Fx name: "..fxname.."\n")
